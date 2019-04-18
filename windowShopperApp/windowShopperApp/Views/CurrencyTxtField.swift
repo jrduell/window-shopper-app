57266f05082e5a6afd113c41b496fa9af1f14e41
Jacob Duell <jrduell7@gmail.com>
2019-04-18 00:44:04 -0500

    lesson-01, creating custom text fields

---
+++ windowShopperApp/windowShopperApp/Views/CurrencyTxtField.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyTxtField.swift
+//  windowShopperApp
+//
+//  Created by jacob on 4/18/19.
+//  Copyright © 2019 jacob. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes:[.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)        }
+        
+    }
+
+}
