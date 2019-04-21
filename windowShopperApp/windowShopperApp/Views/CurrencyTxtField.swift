e801673e041011d78984c41e44b51c76fd5404fc
Jacob Duell <jrduell7@gmail.com>
2019-04-21 16:56:08 -0500

    redo of lesson-01

--- windowShopperApp/windowShopperApp/Views/CurrencyTxtField.swift
+++ windowShopperApp/windowShopperApp/Views/CurrencyTxtField.swift
@@ -16,14 +16,12 @@ class CurrencyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         
-        if placeholder == nil {
-            placeholder = " "
-        }
         
-        if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes:[.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-            attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)        }
+        if let p = placeholder{
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place //attributedPlaceHolder will override placeholder
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
         
     }
 
