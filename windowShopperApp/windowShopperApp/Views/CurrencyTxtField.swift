4dd09513b2b6e974471d9a50d70459cd11a9eb1c
Jacob Duell <jrduell7@gmail.com>
2019-04-22 13:57:45 -0500

    IBDesignable shows programmatic properties in real time, calcbtn added and decimal keyboard

--- windowShopperApp/windowShopperApp/Views/CurrencyTxtField.swift
+++ windowShopperApp/windowShopperApp/Views/CurrencyTxtField.swift
@@ -8,21 +8,36 @@
 
 import UIKit
 
+@IBDesignable
+
 class CurrencyTxtField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+       
+        customizeView()
+    }
+
+   
+    
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
+        //set placeholder to string instead of nil, then convert to NSAttributedString to edit .foregroundColor of placeholder
         
         if let p = placeholder{
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place //attributedPlaceHolder will override placeholder
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
     }
-
+    
 }
+
+
